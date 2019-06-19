aaf8754efd98c3814288b9bf4b15ef70ad96bfa9
Jeffafa1992 <nicholasrocksvold@yahoo.com>
2019-06-19 14:40:38 -0400

    finished spring demo annotations project, moving on to spring mvc

---
+++ SpringDemoAnnotations/src/com/revature/springdemo/SportConfig.java
@@ -0,0 +1,24 @@
+package com.revature.springdemo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+//@ComponentScan("com.revature.springdemo")
+@PropertySource("classpath:sport.properties")
+public class SportConfig {
+	
+	//define bean for sad fortune service
+	@Bean
+	public FortuneService sadFortuneService() {
+		return new SadFortuneService();
+	}
+	
+	//define bean for swim coach and inject dependencies
+	@Bean
+	public Coach swimCoach() {
+		return new SwimCoach(sadFortuneService());
+	}
+
+}
--- SpringDemoAnnotations/src/com/revature/springdemo/TennisCoach.java
+++ SpringDemoAnnotations/src/com/revature/springdemo/TennisCoach.java
@@ -1,18 +1,34 @@
 package com.revature.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Scope("prototype")
 public class TennisCoach implements Coach {
 
 	@Autowired
-	@Qualifier("happyFortuneService")
+	@Qualifier("randomFortuneService")
 	private FortuneService fortuneService;
 	
 	public TennisCoach() {
 		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	//define an init method
+	@PostConstruct
+	public void doMyStartupStuff(){
+		System.out.println(">>TennisCoach: Inside of doMyStartupStuff()");
+	}
+	
+	//define a destroy method
+	@PreDestroy
+	public void doMyCleanupStuff(){
+		System.out.println(">>TennisCoach: Inside of doMyCleanupStuff()");
 	}
 	
 	//define a setter method
